fa28cbce448687ab244858369f8694b31e7bf889
LURADU <48625664+LURADU@users.noreply.github.com>
2022-05-03 20:04:30 +0300

    proiect django

--- proiect/proiect/settings.py
+++ proiect/proiect/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'user_profile.middleware.LogsMiddleware',
 ]
 
 ROOT_URLCONF = 'proiect.urls'
---
+++ proiect/user_profile/urls.py
@@ -0,0 +1,10 @@
+from django import views
+from django.urls import path
+
+
+app_name = 'profile'
+
+urlpatterns = [
+    path('', ),
+
+]
