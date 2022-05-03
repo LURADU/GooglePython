e2e7788434baee455a0ac8c199c51fd7fd744747
LURADU <48625664+LURADU@users.noreply.github.com>
2022-05-03 19:01:13 +0300

    proiect django

--- proiect/proiect/settings.py
+++ proiect/proiect/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from templates.secret_key import SECRET_KEY_VALUE
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4h1%h#m9oiu1qksvzgsz&7%gl$ub6syc9ku25%+vubyg45s*m5'
-
+SECRET_KEY = SECRET_KEY_VALUE
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
