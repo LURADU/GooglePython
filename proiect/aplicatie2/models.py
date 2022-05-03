f3d7b64c200d04188eba6ea9c641b4279eba73ee
LURADU <48625664+LURADU@users.noreply.github.com>
2022-05-03 18:47:30 +0300

    proiect django

---
+++ proiect/aplicatie2/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+tipuri_companiei = (("SRL","S.R.L."),("SA","S.A."))
+
+
+class Companies(models.Model):
+    nume = models.CharField(max_length=100)
+    website = models.CharField(max_length=50)
+    tip_companie = models.CharField(max_length=10, choices=tipuri_companiei)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.nume}-{self.tip_companie}'
+
+
